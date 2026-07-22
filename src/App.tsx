8c4d03894c5869b3b44811f6f369464a98522030
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2026-07-22 16:50:13 -0400

    Configurações iniciais do heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Heading } from './components/Heading';
 
+import './styles/global.css';
+import './styles/theme.css';
+
+import { TimerIcon } from 'lucide-react';
+
 export function App() {
    return (
       <>
-         <Heading />
+         <Heading>
+            Olá Mundo!
+            <button>
+               <TimerIcon />
+            </button>
+         </Heading>
          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
             rerum excepturi error amet et vero, eaque tempore similique, quae
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+// import styles from './styles/global.css';
+
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
